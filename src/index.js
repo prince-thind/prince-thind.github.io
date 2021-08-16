e833604a96988483875a3dddb2c51c121aa152b2
Prince-Thind <princethind799@gmail.com>
2021-08-16 19:36:12 +0530

    crude build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './styles/index.css';
 import './styles/effects.css';
+import '../node_modules/@fortawesome/fontawesome-free/js/all'
 
 import typewrite from './components/typewriter';
 import fadeText from './components/fadeText';
+import './components/scroll';
+
 
 const intro = document.querySelector('#welcome-heading');
 const subHeading = document.querySelector('#welcome-sub-heading');
