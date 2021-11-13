257d2c57c794f883173cbf04bfc87be005d94c83
Prince-Thind <princethind799@gmail.com>
2021-11-13 12:54:57 +0530

    implement basic intro

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import styles from "../styles/Home.module.scss";
+
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.home}>
+      <section className={styles.intro}>
+        <h1>Hi, I am Prince Thind</h1>
+        <h2>A Full stack Web Developer</h2>
+        <hr />
+      </section>
     </div>
   );
 }
