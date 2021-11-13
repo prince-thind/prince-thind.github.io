80d658acaba8a80b695a13e79f1c61cf5602c70c
Prince-Thind <princethind799@gmail.com>
2021-11-13 10:56:28 +0530

    implement basic header

--- components/header.js
+++ components/header.js
@@ -1,5 +1,38 @@
-export default function Header(){
-    return <header>
-        <h1>Header content</h1>
+import Link from "next/link";
+import styles from "../styles/Header.module.scss";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            <Link href="/">
+              <a className={styles["nav-item"]}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects">
+              <a className={styles["nav-item"]}>Projects</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/education">
+              <a className={styles["nav-item"]}>Education</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/achievements">
+              <a className={styles["nav-item"]}>Achievements</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a className={styles["nav-item"]}>Contact Me</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
+  );
 }
