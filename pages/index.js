86d4e21284545f0b6735553c10da5df3f49fa21e
Prince-Thind <princethind799@gmail.com>
2021-11-14 16:49:56 +0530

    finish basic index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,47 @@
 import styles from "../styles/Home.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import profilePic from "../images/brand-logo.png";
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <>
       <section className={styles.intro}>
         <h1>Hi, I am Prince Thind</h1>
-        <h2>A Full stack Web Developer</h2>
+        <h2>A Full Stack Web Developer</h2>
         <hr />
       </section>
-    </div>
+      <section className={styles["main-section"]}>
+        <Image
+          src={profilePic}
+          alt="profile picture"
+          height="150"
+          width="150"
+        />
+        <div className={styles["details"]}>
+          <p>Hello, I am Prince Thind, a Full Stack web developer and Programmer working to create the best websites with modern technologies like React and ExpressJS. Visit my Dedicated <Link href="/about"><a>about</a></Link> Page to know more about me.</p>
+          <ul>
+            <li>
+              <Link href="/projects">
+                <a>Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/education">
+                <a>Education</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <a href="https://github.com/Prince-Thind">Github</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
   );
 }
