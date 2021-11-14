bffd2ddc1f619ee77a3f252465b37b6cf71b72f9
Prince-Thind <princethind799@gmail.com>
2021-11-14 12:39:32 +0530

    add brand image in header

--- components/header.js
+++ components/header.js
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import Image from "next/image";
+import brandLogo from "../images/brand-logo.png";
 import styles from "../styles/Header.module.scss";
 
 export default function Header() {
   return (
     <header>
       <nav className={styles.navbar}>
+        <Link href="/">
+          <a className={styles.brand}>
+            <Image
+              src={brandLogo}
+              alt="brand logo"
+              width="50"
+              height="50"
+              className="brand-logo"
+            />
+            <h2>
+              Prince
+              <br />
+              Thind
+            </h2>
+          </a>
+        </Link>
         <ul>
           <li>
             <Link href="/">
