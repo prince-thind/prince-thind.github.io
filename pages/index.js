10083c17a8f8e271074675809433a0195004cb88
Prince-Thind <princethind799@gmail.com>
2021-11-15 19:48:24 +0530

    update styles for homepage

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import profilePic from "../images/brand-logo.png";
 import Head from "next/head";
-import Resume from '../components/resume'
+import Resume from "../components/resume";
 
 export default function Home() {
   return (
     <>
-    <Head>
+      <Head>
         <title>Home</title>
       </Head>
       <section className={styles.intro}>
@@ -21,31 +21,44 @@ export default function Home() {
         <Image
           src={profilePic}
           alt="profile picture"
-          height="150"
-          width="150"
+          height="200"
+          width="200"
         />
-        <div className={styles["details"]}>
-          <p>Hello, I am Prince Thind, a Full Stack web developer and Programmer working to create the best websites with modern technologies like React and ExpressJS. Visit my Dedicated <Link href="/about"><a>about</a></Link> Page to know more about me.</p>
-          <ul className={styles["details-list"]}>
-            <li>
-              <Link href="/work">
-                <a>work</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/education">
-                <a>Education</a>
-              </Link>
-            </li>
-            <li>
+        <div className={styles["brief"]}>
+          <h3>About me</h3>
+          <div className={styles["details"]}>
+            <p>
+              Hello, I am Prince Thind, a Full Stack web developer and
+              Programmer working to create the best websites with modern
+              technologies like React and ExpressJS. Currently Pursuing B.Tech
+              CSE(Hons.) at LPU and looking for internship opportunities. Visit
+              my dedicated{" "}
               <Link href="/about">
-                <a>About</a>
-              </Link>
-            </li>
-            <li>
-              <a href="https://github.com/Prince-Thind">Github</a>
-            </li>
-          </ul>
+                <a>about</a>
+              </Link>{" "}
+              page to know more about me.
+            </p>
+            <ul className={styles["details-list"]}>
+              <li>
+                <Link href="/work">
+                  <a>work</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/education">
+                  <a>Education</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </li>
+              <li>
+                <a href="https://github.com/Prince-Thind">Github</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </>
