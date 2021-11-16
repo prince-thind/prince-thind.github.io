520dc5f4402981a439dd15f2f4c02f8a28c33e2c
Prince-Thind <princethind799@gmail.com>
2021-11-16 21:46:48 +0530

    update collapse bar code

--- components/collapase-bar.js
+++ components/collapase-bar.js
@@ -1,29 +1,22 @@
 import { useState } from "react";
 import styles from "../styles/Collapse-bar.module.scss";
 
-
 function CollapseBar(props) {
   const [collpased, setCollapased] = useState(true);
 
-  const heading = (
-    <h2
-    className={styles["collapse-bar"]}
-      onClick={() => {
-        setCollapased((collpased) => !collpased);
-      }}
-    >
-        {collpased?"+":"-"}
-        {"  "}
-      {props.topic}
-    </h2>
-  );
-  if (collpased) {
-    return heading;
-  }
   return (
     <>
-      {heading}
-      {props.children}
+      <h2
+        className={styles["collapse-bar"]}
+        onClick={() => {
+          setCollapased((collpased) => !collpased);
+        }}
+      >
+        {collpased ? "+ " : "- "}
+        {props.topic}
+      </h2>
+      <div className={collpased?styles["hidden"]:""}> {props.children}</div>
+     
     </>
   );
 }
