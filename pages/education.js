72019eb63299da6e2d897fbf918a408a804980fa
Prince-Thind <princethind799@gmail.com>
2021-11-16 16:22:48 +0530

    implement basic collapse bar

--- pages/education.js
+++ pages/education.js
@@ -1,33 +1,46 @@
 import Head from "next/head";
 import CollapseBar from "../components/collapase-bar";
-import styles from '../styles/Education.module.scss'
+import styles from "../styles/Education.module.scss";
 
 export default function Education() {
-    return (
-      <div><Head>
-      <title>Education</title>
-    </Head>
-       <section className={styles["education"]}>
-         <h2>Education</h2>
-         <ul className={styles["education-list"]}>
-         <li className={styles["education-list-item"]}>
-                <div className={styles["education-institute"]}>
-                    <div className={styles["education-main"]}>
-                        <div className={styles["education-title"]}> B.Tech in CSE (Hons.)</div>
-
-                        <div className={styles["education-institute-name"]}>(Lovely Professional University)</div>
-                    </div>
-                    <div className={styles["education-institute-duration"]}>2019-2023</div>
+  return (
+    <div>
+      <Head>
+        <title>Education</title>
+      </Head>
+      <section className={styles["education"]}>
+        <CollapseBar topic="Education">
+        <ul className={styles["education-list"]}>
+          <li className={styles["education-list-item"]}>
+            <div className={styles["education-institute"]}>
+              <div className={styles["education-main"]}>
+                <div className={styles["education-title"]}>
+                  {" "}
+                  B.Tech in CSE (Hons.)
                 </div>
-                <div className={styles["education-institute-grades"]}>CGPA: ~9</div>
-                <ul className={styles["education-institute-details"]}>
-                    <li className={styles["education-institure-points"]}>Web Development as Egnineering Minor</li>
-                    <li className={styles["education-institure-points"]}>Digital Marketing as Open Minor</li>
-                </ul>
-            </li>
-         </ul>
-       </section>
-      </div>
-    );
-  }
-  
+
+                <div className={styles["education-institute-name"]}>
+                  (Lovely Professional University)
+                </div>
+              </div>
+              <div className={styles["education-institute-duration"]}>
+                2019-2023
+              </div>
+            </div>
+            <div className={styles["education-institute-grades"]}>CGPA: ~9</div>
+            <ul className={styles["education-institute-details"]}>
+              <li className={styles["education-institure-points"]}>
+                Web Development as Egnineering Minor
+              </li>
+              <li className={styles["education-institure-points"]}>
+                Digital Marketing as Open Minor
+              </li>
+            </ul>
+          </li>
+        </ul>
+        </CollapseBar>
+       
+      </section>
+    </div>
+  );
+}
