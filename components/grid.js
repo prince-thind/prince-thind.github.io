fcb5a7831e368b9ee27b3ed221772e839bdc031f
Prince-Thind <princethind799@gmail.com>
2021-11-16 21:09:00 +0530

    style certification grid

--- components/grid.js
+++ components/grid.js
@@ -1,16 +1,29 @@
 import Image from "next/image";
+import styles from "../styles/Grid.module.scss";
 
-function Grid({ projects,type }) {
-  return <ul>{projects.map((project,i) => {
-      return <li key={i}>
-          <h4>{project.name}</h4>
-          <Image src={project.image} height="500" width="500" alt="project"/>
-          <div className="links"><a href={project.link}>Link</a>
-          {type=="github"?<a href={project.link}>Live Link</a>:null}
-          </div>
-          
-      </li>
-  })}</ul>;
+function Grid({ projects, type }) {
+  if (type === "github") return null;
+  if (type === "certification") {
+    return (
+      <ul className={styles["grid"]}>
+        {projects.map((project, i) => {
+          return (
+            <li key={i} className={styles["grid-item"]}>
+              <h4>{project.name}</h4>
+            <a href={project.link}>  <Image
+                src={project.image}
+                height="500"
+                width="500"
+                alt="project"
+              />
+                </a>
+               
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default Grid;
