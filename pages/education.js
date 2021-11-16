f5312fb4f3e7f0a4842d36486644b5de0ad3dddd
Prince-Thind <princethind799@gmail.com>
2021-11-16 15:37:51 +0530

    education bar dummy setup

--- pages/education.js
+++ pages/education.js
@@ -1,12 +1,32 @@
 import Head from "next/head";
-
+import CollapseBar from "../components/collapase-bar";
+import styles from '../styles/Education.module.scss'
 
 export default function Education() {
     return (
       <div><Head>
       <title>Education</title>
     </Head>
-        Education
+       <section className={styles["education"]}>
+         <h2>Education</h2>
+         <ul className={styles["education-list"]}>
+         <li className={styles["education-list-item"]}>
+                <div className={styles["education-institute"]}>
+                    <div className={styles["education-main"]}>
+                        <div className={styles["education-title"]}> B.Tech in CSE (Hons.)</div>
+
+                        <div className={styles["education-institute-name"]}>(Lovely Professional University)</div>
+                    </div>
+                    <div className={styles["education-institute-duration"]}>2019-2023</div>
+                </div>
+                <div className={styles["education-institute-grades"]}>CGPA: ~9</div>
+                <ul className={styles["education-institute-details"]}>
+                    <li className={styles["education-institure-points"]}>Web Development as Egnineering Minor</li>
+                    <li className={styles["education-institure-points"]}>Digital Marketing as Open Minor</li>
+                </ul>
+            </li>
+         </ul>
+       </section>
       </div>
     );
   }
