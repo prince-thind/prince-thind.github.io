03578ef16b5ac2b1dff1a6d901c1e47d0b7e017d
Prince-Thind <princethind799@gmail.com>
2021-11-17 16:41:07 +0530

    update project related code

--- components/grid.js
+++ components/grid.js
@@ -1,23 +1,25 @@
 import Image from "next/image";
 import styles from "../styles/Grid.module.scss";
+import uniqid from 'uniqid'
 
 function Grid({ projects, type }) {
   if (type === "github")
     return (
-      <ul className={styles["grid"]}>
-        {projects.map((project, i) => {
+      <ul className={styles["grid-projects"]}>
+        {projects.map((project) => {
           return (
-            <li key={i} className={styles["grid-item"]}>
+            <li key={uniqid()} className={styles["grid-item"]}>
               <h4>{project.name}</h4>
-              <a href={project.link}>
+              <a href={project.live_link}>
                 {" "}
                 <Image
                   src={project.image}
-                  height="500"
-                  width="500"
+                  height="450"
+                  width="800"
                   alt="project"
                 />
               </a>
+              <a href={project.link} className={styles["source-link"]}>View Source on Github</a>
             </li>
           );
         })}
@@ -26,9 +28,9 @@ function Grid({ projects, type }) {
   if (type === "certification") {
     return (
       <ul className={styles["grid"]}>
-        {projects.map((project, i) => {
+        {projects.map((project) => {
           return (
-            <li key={i} className={styles["grid-item"]}>
+            <li key={uniqid()} className={styles["grid-item"]}>
               <h4>{project.name}</h4>
               <a href={project.link}>
                 {" "}
