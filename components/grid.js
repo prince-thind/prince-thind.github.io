f3724362078d794972303ff3ceb09ef9bb16403f
Prince-Thind <princethind799@gmail.com>
2021-11-17 16:24:18 +0530

    implement basic render for projects

--- components/grid.js
+++ components/grid.js
@@ -2,7 +2,27 @@ import Image from "next/image";
 import styles from "../styles/Grid.module.scss";
 
 function Grid({ projects, type }) {
-  if (type === "github") return null;
+  if (type === "github")
+    return (
+      <ul className={styles["grid"]}>
+        {projects.map((project, i) => {
+          return (
+            <li key={i} className={styles["grid-item"]}>
+              <h4>{project.name}</h4>
+              <a href={project.link}>
+                {" "}
+                <Image
+                  src={project.image}
+                  height="500"
+                  width="500"
+                  alt="project"
+                />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    );
   if (type === "certification") {
     return (
       <ul className={styles["grid"]}>
@@ -10,14 +30,15 @@ function Grid({ projects, type }) {
           return (
             <li key={i} className={styles["grid-item"]}>
               <h4>{project.name}</h4>
-            <a href={project.link}>  <Image
-                src={project.image}
-                height="500"
-                width="500"
-                alt="project"
-              />
-                </a>
-               
+              <a href={project.link}>
+                {" "}
+                <Image
+                  src={project.image}
+                  height="500"
+                  width="500"
+                  alt="project"
+                />
+              </a>
             </li>
           );
         })}
--- pages/work.js
+++ pages/work.js
@@ -1,12 +1,25 @@
 import Head from "next/head";
+import projects from "../lib/projects";
+import CollapseBar from "../components/collapase-bar";
+import Grid from "../components/grid";
+import uniqid from "uniqid";
 
 export default function Work() {
+  const types = Object.keys(projects);
+
   return (
     <div>
       <Head>
         <title>Work</title>
       </Head>
-      Work
+      <h2>Projects</h2>
+      {types.map((type) => {
+        return (
+          <CollapseBar topic={type} key={uniqid()}>
+            <Grid projects={projects[type]} type="github" />
+          </CollapseBar>
+        );
+      })}
     </div>
   );
 }
