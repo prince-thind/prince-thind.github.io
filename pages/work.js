65a6991d13fa066ed224a247c18a3ddb16dd60d8
Prince-Thind <princethind799@gmail.com>
2021-11-20 13:59:15 +0530

    add skillset collpase bar

--- pages/work.js
+++ pages/work.js
@@ -3,11 +3,13 @@ import projects from "../lib/projects";
 import CollapseBar from "../components/collapase-bar";
 import Grid from "../components/grid";
 import uniqid from "uniqid";
-
+import skills from "../lib/skills";
+import styles from "../styles/Work.module.scss";
 
 
 export default function Work() {
   const types = Object.keys(projects);
+  const skillsList = Object.keys(skills);
 
   return (
     <div>
@@ -15,13 +17,26 @@ export default function Work() {
         <title>Work</title>
       </Head>
       <CollapseBar topic="Projects" type="big">
-      {types.map((type) => {
-        return (
-          <CollapseBar topic={type} key={uniqid()} type="children">
-            <Grid projects={projects[type]} type="github" />
-          </CollapseBar>
-        );
-      })}
+        {types.map((type) => {
+          return (
+            <CollapseBar topic={type} key={uniqid()} type="children">
+              <Grid projects={projects[type]} type="github" />
+            </CollapseBar>
+          );
+        })}
+      </CollapseBar>
+      <CollapseBar topic="Skills" type="big">
+        {skillsList.map((skill) => {
+          return (
+            <CollapseBar topic={skill} key={uniqid()} type="children">
+              <ul className={styles["skills-list"]}>
+                {skills[skill].map((subSkill) => {
+                  return <li key={uniqid()}>{subSkill}</li>;
+                })}
+              </ul>
+            </CollapseBar>
+          );
+        })}
       </CollapseBar>
     </div>
   );
