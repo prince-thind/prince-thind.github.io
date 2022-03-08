aa44285fe15f97b40d7313d2e0433ea8cf8e586a
Prince Thind <Princethind799@gmail.com>
2022-03-08 21:11:37 +0530

    update contact details

--- components/resume.js
+++ components/resume.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Resume() {
   return (
-    <a className={styles.resume} href="/CV.pdf" download>
+    <a className={styles.resume} href="https://github.com/prince-thind/resume/raw/master/CV.pdf" download>
       Download CV
       <FontAwesomeIcon
         icon={faArrowCircleDown}
