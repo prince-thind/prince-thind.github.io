acc889a45d61cccafabc9eecf6c657b51cd8cda7
Prince Thind <princethind799@gmail.com>
2024-05-11 12:50:48 -0400

    fix broken submit button

--- src/lib/form-submit.js
+++ src/lib/form-submit.js
@@ -1,62 +1,65 @@
-const weburl = 'https://script.google.com/macros/s/AKfycbwE8pVi0ioOXZ7j_-t6G6P0rnlkLsemiMOOwYSKvpcyJik4MbYgyMKuDNHN2Kp0Sjo3ZQ/exec'
-
+// const weburl = 'https://script.google.com/macros/s/AKfycbwE8pVi0ioOXZ7j_-t6G6P0rnlkLsemiMOOwYSKvpcyJik4MbYgyMKuDNHN2Kp0Sjo3ZQ/exec'
+const weburl =
+  "https://script.google.com/macros/s/AKfycbwYAXPWTn_EjnQrD-M27GRksLh3SkqKaw81ZDCaW4irxRS9ivQaJuR6wCrBdQc9zp6H/exec";
 
 let submiitedAlready = false;
 
-
 export default function LinkFormWithGoogleSheet() {
-    const form = document.querySelector('#contact-form');
-    form.addEventListener('submit', handleFormSubmit)
+  const form = document.querySelector("#contact-form");
+  form.addEventListener("submit", handleFormSubmit);
 }
 
 async function handleFormSubmit(e) {
-    e.preventDefault();
-    if (submiitedAlready) {
-        displayFormResult({ style: 'error', message: 'You have already filled this form once!', });
-        return;
-    }
-    submiitedAlready = true;
+  e.preventDefault();
+  if (submiitedAlready) {
+    displayFormResult({
+      style: "error",
+      message: "You have already filled this form once!",
+    });
+    return;
+  }
+  submiitedAlready = true;
 
-    displayFormResult({ message: 'Submitting Response...', style: null })
+  displayFormResult({ message: "Submitting Response...", style: null });
 
+  const data = Object.fromEntries(new FormData(e.target));
+  const success = await postData(data);
 
-    const data = Object.fromEntries(new FormData(e.target));
-    const success = await postData(data);
-
-    if (success) {
-        displayFormResult({ message: 'Data Submitted Successfully!', style: 'success' })
-    }
-    else {
-        displayFormResult({ message: 'Some Unknown Error Occured, please contact me via other means', style: 'error' })
-
-    }
+  if (success) {
+    displayFormResult({
+      message: "Data Submitted Successfully!",
+      style: "success",
+    });
+  } else {
+    displayFormResult({
+      message: "Some Unknown Error Occured, please contact me via other means",
+      style: "error",
+    });
+  }
 }
 
 function displayFormResult(data) {
-    const formResultDiv = document.querySelector('#form-result');
-    formResultDiv.textContent = data.message;
+  const formResultDiv = document.querySelector("#form-result");
+  formResultDiv.textContent = data.message;
 
-    if (data.style == 'error') {
-        formResultDiv.classList.add('error');
-    }
-    else if (data.style == 'success') {
-        formResultDiv.classList.add('success');
-    }
+  if (data.style == "error") {
+    formResultDiv.classList.add("error");
+  } else if (data.style == "success") {
+    formResultDiv.classList.add("success");
+  }
 }
 
 async function postData(data) {
-    const rawRes = await fetch(weburl, {
-        method: 'POST',
-        body: new URLSearchParams(data)
-    })
+  const rawRes = await fetch(weburl, {
+    method: "POST",
+    body: new URLSearchParams(data),
+  });
 
-    const res = await rawRes.json();
+  const res = await rawRes.json();
 
-    if (res.result == 'success') {
-        return true;
-    }
+  if (res.result == "success") {
+    return true;
+  }
 
-    return false;
-
-
+  return false;
 }
