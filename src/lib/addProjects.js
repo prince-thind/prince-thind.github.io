46e1da6f67e9ec028f8f2f85e0b1769e2b202e47
Prince Thind <princethind799@gmail.com>
2024-05-11 11:46:14 -0400

    translate to french

--- src/lib/addProjects.js
+++ src/lib/addProjects.js
@@ -1,30 +1,36 @@
-import projects from '../data/projects.json'
+import projects from "../data/projects.json";
 
 export default function addProjects() {
-    const projectsTab = document.querySelector('#projects-tab')
-    for (const project of projects) {
-        const projectLi = createProjectLi(project);
-        projectsTab.appendChild(projectLi)
-    }
+  const projectsTab = document.querySelector("#projects-tab");
+  for (const project of projects) {
+    const projectLi = createProjectLi(project);
+    projectsTab.appendChild(projectLi);
+  }
 }
 
 function createProjectLi(project) {
-    const li = document.createElement('li');
-    li.classList.add('project');
-    li.innerHTML = `
-    <img src="${getImageUrl(project.imageSrc)}" alt="sample-project" class="project-image">
+  const li = document.createElement("li");
+  li.classList.add("project");
+  li.innerHTML = `
+    <img src="${getImageUrl(
+      project.imageSrc
+    )}" alt="sample-project" class="project-image">
     <div class="project-info">
         <h4 class="project-heading">${project.name}</h4>
         <p class="project-description">${project.description}</p>
          <div class="project-links">
-             <a href="${project.liveLink}" class="project-link plain-link link-button" rel="noreferrer noopener" target="_blank">Live Link <i class="fa-solid icon fa-arrow-up-right-from-square"></i> </a>
-             <a href="${project.sourceLink}"  class="project-link plain-link link-button" rel="noreferrer noopener" target="_blank">Source Link <i class="fa-solid icon fa-arrow-up-right-from-square"></i> </a>
+             <a href="${
+               project.liveLink
+             }" class="project-link plain-link link-button" rel="noreferrer noopener" target="_blank">Lien direct<i class="fa-solid icon fa-arrow-up-right-from-square"></i> </a>
+             <a href="${
+               project.sourceLink
+             }"  class="project-link plain-link link-button" rel="noreferrer noopener" target="_blank">Lien source<i class="fa-solid icon fa-arrow-up-right-from-square"></i> </a>
           </div>
-    </div>`
+    </div>`;
 
-    return li;
+  return li;
 }
 
 function getImageUrl(name) {
-    return new URL(`../assets/project-images/${name}`, import.meta.url).href
+  return new URL(`../assets/project-images/${name}`, import.meta.url).href;
 }
