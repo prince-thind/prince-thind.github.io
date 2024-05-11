1f8ac4567fa6c915b46e17170d52f2f839ec4c1d
Prince Thind <princethind799@gmail.com>
2024-05-11 12:17:15 -0400

    fix links and profile picture

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import './stylesheets/main.css'
-import './stylesheets/header.css'
-import './stylesheets/utility.css'
-import './stylesheets/intro.css'
-import './stylesheets/pfp.css'
-import './stylesheets/work.css'
-import './stylesheets/about.css'
-import './stylesheets/footer.css'
-import './stylesheets/contact.css'
-import './stylesheets/projects.css'
+import "./stylesheets/main.css";
+import "./stylesheets/header.css";
+import "./stylesheets/utility.css";
+import "./stylesheets/intro.css";
+import "./stylesheets/pfp.css";
+import "./stylesheets/work.css";
+import "./stylesheets/about.css";
+import "./stylesheets/footer.css";
+import "./stylesheets/contact.css";
+import "./stylesheets/projects.css";
 
-import addProjects from './lib/addProjects.js';
-import addLinks from './lib/addLinks';
-import addCSSDesigns from './lib/cssDesigns.js'
-import LinkFormWithGoogleSheet from './lib/form-submit'
-import startImageAnimation from './lib/startImageAnimation'
+import addProjects from "./lib/addProjects.js";
+import addLinks from "./lib/addLinks";
+import addCSSDesigns from "./lib/cssDesigns.js";
+import LinkFormWithGoogleSheet from "./lib/form-submit";
+// import startImageAnimation from "./lib/startImageAnimation";
 
-
-
-addProjects()
-addLinks()
-addCSSDesigns()
-LinkFormWithGoogleSheet()
-startImageAnimation()
+addProjects();
+addLinks();
+addCSSDesigns();
+LinkFormWithGoogleSheet();
+// startImageAnimation()
